b56b3e6f812d77e79ed1018d8b50bacbe8a34c99
Bahri Meriç Canlı <bahri@bahri.info>
2026-07-16 11:16:52 +0300

    fix: emoji script/style kaldır (wp-emoji-release.min.js 404)

--- functions.php
+++ functions.php
@@ -621,4 +621,12 @@ function ubuntucommunity_featured_slider() {
 	echo '<nav id="uc-slider-controllers"></nav>';
 	echo '</section>';
 }
+
+// Emoji script/style'larını kaldır (wp-emoji-release.min.js 404 önlemi)
+add_action( 'init', function () {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+} );
 ?>
